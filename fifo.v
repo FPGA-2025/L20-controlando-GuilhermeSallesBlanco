f13ea8686933b46e1c8dbbdf4c949eecee7fc073
GuilhermeSallesBlanco <guilherme.salles.blanco@gmail.com>
2025-06-01 02:53:06 -0300

    Adicionando tarefa realizada

--- fifo.v
+++ fifo.v
@@ -1,21 +1,51 @@
 module fifo(
-    input   clk,
-    input   rst_n,
+    input        clk,
+    input        rst_n,
 
     // Write interface
-    input   wr_en,
-    input   [7:0] data_in,
-    output  full,
+    input        wr_en,
+    input  [7:0] data_in,
+    output       full,
 
     // Read interface
-    input   rd_en,
-    output  reg [7:0] data_out,
-    output  empty,
+    input        rd_en,
+    output reg [7:0] data_out,
+    output       empty,
 
     // status
     output reg [3:0] fifo_words  // Current number of elements
 );
 
-//insira seu código aqui
+parameter TAM_DADO = 8;
+parameter PROF_FIFO = 8;
+
+reg [TAM_DADO-1:0] fifo_mem [0:PROF_FIFO-1]; 
+reg [TAM_DADO-1:0] wr_ptr; 
+reg [TAM_DADO-1:0] rd_ptr;
+
+assign full = ((wr_ptr == rd_ptr -1) || (wr_ptr == PROF_FIFO && rd_ptr == 0));
+assign empty = (wr_ptr == rd_ptr);
+
+always @(posedge clk) begin
+    if(!rst_n) begin
+        wr_ptr <= 0;
+    end else if(wr_en && !full) begin
+        fifo_mem[wr_ptr] <= data_in;
+        wr_ptr <= wr_ptr + 1;
+    end 
+end
+
+always @(posedge clk) begin
+    if(!rst_n) begin
+        rd_ptr <= 0;
+    end else if(rd_en && !empty) begin
+        data_out <= fifo_mem[rd_ptr];
+        rd_ptr <= rd_ptr + 1;
+    end 
+end
+
+always @(*) begin
+    fifo_words = wr_ptr - rd_ptr;
+end
 
 endmodule
--- fsm.v
+++ fsm.v
@@ -1,14 +1,61 @@
 module fsm(
-    input   clk,
-    input   rst_n,
+    input        clk,
+    input        rst_n,
 
-    output reg wr_en,
-
-    output [7:0] fifo_data,
-    
-    input [3:0] fifo_words
+    output reg   wr_en,       // controla a escrita na FIFO
+    output [7:0] fifo_data,   // sempre 0xAA
+    input  [3:0] fifo_words   // quantas palavras estão na FIFO
 );
 
-// insira seu código aqui
+localparam STOP = 2'b00;
+localparam WRITING = 2'b01;
+localparam READING = 2'b10;
+
+reg [1:0] estado, prox_estado;
+reg [7:0] fifo_data_reg;
+
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n && estado !== STOP) begin
+        estado <= STOP;
+    end else begin
+        estado <= prox_estado;
+    end
+end
+
+always @(*) begin
+    prox_estado = estado;
+    case(estado)
+        STOP: begin
+            prox_estado = WRITING;
+        end
+        WRITING: begin
+            if(fifo_words >= 5) begin
+                prox_estado = READING;
+            end else begin
+                prox_estado = WRITING;
+            end
+        end
+        READING: begin
+            if(fifo_words <= 2) begin
+                prox_estado = WRITING;
+            end else begin
+                prox_estado = READING;
+            end
+        end
+        default: begin
+            prox_estado = STOP;
+        end
+    endcase
+end
+
+assign fifo_data = fifo_data_reg;
+
+always @(*) begin
+    fifo_data_reg = 8'hAA; 
+    case(estado)
+        WRITING: wr_en = 1;
+        READING: wr_en = 0;
+    endcase
+end
 
 endmodule
